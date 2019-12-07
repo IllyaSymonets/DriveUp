5e60d1cdc269f142befe8f8bc9561e2b3e81ff1c
IllyaSymonets <illya.symonets@gmail.com>
2019-12-07 16:18:57 +0200

    OrderService changes
    App was added
    
    OrderRepo:
     extraMethods was added(findAllByStatus, findAllByDriverId, findAllByCustomerId)
    
     OrderController was added
     OrderViewsController was added

--- BE/OrderService/src/main/java/softserve/academy/repos/OrderRepo.java
+++ BE/OrderService/src/main/java/softserve/academy/repos/OrderRepo.java
@@ -2,8 +2,16 @@ package softserve.academy.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import softserve.academy.domain.Order;
+import softserve.academy.domain.OrderStatus;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepo extends JpaRepository<Order, UUID> {
+
+    List<Order> findAllByStatus(OrderStatus status);
+
+    List<Order> findAllByDriverId(UUID driverId);
+
+    List<Order> findAllByCustomerId(UUID customerId);
 }
