0cce4de9d96b54a5b7cc20a8bec7feedc702365c
IllyaSymonets <illya.symonets@gmail.com>
2019-12-22 21:48:24 +0200

    driverSideInterface

---
+++ UI/DriveUp_latest/scripts/orderScript.js
@@ -0,0 +1,28 @@
+const vOrder = new Vue({
+    el: '#DriveUp-menu',
+    data:{
+        RoleToChange:'driver',
+        RoleToChangeIcon:'local_taxi',
+        window:'after-order',
+    }
+    ,
+    methods:{
+        changeRole: function(){
+            if(this.RoleToChange === 'driver'){
+                this.RoleToChange='customer';
+                this.RoleToChangeIcon='airline_seat_recline_normal';
+            }else{
+                this.RoleToChange='driver';
+                this.RoleToChangeIcon='local_taxi';
+            }
+        }
+        ,
+        goToWindow: function(where){
+           this.window=where;
+        }
+        ,
+        onMain: function() {
+            location.replace("orderList.html");
+        }
+    }
+})
