ff36d73648f1e9c01beb1679b761cc00fb3be420
zaets39 <katezytchuk24@gmail.com>
2020-01-18 13:52:42 +0200

    Some Request classes added to billing

---
+++ BE/BillingService/src/main/java/com/driveUp/requests/CreateBillRequest.java
@@ -0,0 +1,10 @@
+package com.driveUp.requests;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateBillRequest {
+    private long orderNumber;
+    private CreateBill billInfo;
+}
